6b48c11c94bd2a90aa3efabf7a9f9cab2d0d443a
Daffadon <daffaputranarendra9@gmail.com>
2024-10-10 19:17:40 +0700

    refactor(entity): change User Struct

--- entity/user_entity.go
+++ entity/user_entity.go
@@ -6,10 +6,10 @@ type User struct {
 	ID         uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
 	Name       string    `json:"name"`
 	Email      string    `json:"email"`
-	Password   string    `json:"password"`
+	Password   string    `json:"password" gorm:"type:varchar(255)"`
 	TelpNumber string    `json:"telp_number"`
-	Age        int       `json:"age"`
-	Role       string    `json:"role"`
+	Age        uint8     `json:"age"`
+	Role       string    `json:"role" gorm:"default:'user'"`
 	IsVerified bool      `json:"is_verified"`
 
 	TimeStamp
